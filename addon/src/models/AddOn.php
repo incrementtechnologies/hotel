6ed2ee670ff43088fd3f9f851073beb1e8fd1898
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-09-17 17:33:40 +0800

    add-ons

--- addon/src/models/AddOn.php
+++ addon/src/models/AddOn.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class AddOn extends Model
 {
-    //
+    protected $table = 'add-ons';
+    protected $fillable = ['merchant_id', 'account_id', 'title',  'currency', 'price', 'url'];
 }
