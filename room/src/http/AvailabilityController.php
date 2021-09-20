e8c17655215ec5e432db03cffe5d9ab400cfc684
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-09-20 14:04:35 +0800

    initial

---
+++ room/src/http/AvailabilityController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Increment\Hotel\Room\Http;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\APIController;
+use Increment\Hotel\Room\Models\Availability;
+
+class AvailabilityController extends APIController
+{
+
+}
