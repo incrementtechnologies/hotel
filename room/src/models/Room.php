a8f0e85ff3a14c08d7e7e63aa70d97e9fcc0d8dd
chloe868 <cherrymae.herrera@student.passerellesnumeriques.org>
2021-09-23 08:25:38 +0800

    Updates

--- room/src/models/Room.php
+++ room/src/models/Room.php
@@ -5,6 +5,6 @@ use Illuminate\Database\Eloquent\Model;
 use App\APIModel;
 class Room extends APIModel
 {
-    protected $table = 'products';
+    protected $table = 'rooms';
     protected $fillable = ['code', 'account_id', 'title',  'description', 'category', 'status'];
 }
