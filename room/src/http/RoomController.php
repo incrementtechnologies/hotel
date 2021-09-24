e43289c04643027fb483ea35c45e21a25502912e
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-09-24 09:42:41 +0800

    retrieve rooms

--- room/src/http/RoomController.php
+++ room/src/http/RoomController.php
@@ -11,4 +11,29 @@ class RoomController extends APIController
   function __construct(){
     $this->model = new Room;
   }
+
+  public function retrieve(Request $request){
+    $data = $request->all();
+    $con = $data['condition'];
+    $result = Room::where($con[0]['column'], $con[0]['clause'], $con[0]['value'])
+      ->where('deleted_at', '=', null)
+      ->limit($data['limit'])
+      ->offset($data['offset'])
+      ->orderBy(array_keys($data['sort'])[0], array_values($data['sort'])[0])
+      ->get();
+    $size = Room::where($con[0]['column'], $con[0]['clause'], $con[0]['value'])
+      ->where('deleted_at', '=', null)
+      ->orderBy(array_keys($data['sort'])[0], array_values($data['sort'])[0])
+      ->get();
+
+    for ($i=0; $i <= sizeof($result)-1 ; $i++) { 
+      $item = $result[$i];
+      $result[$i]['category'] = app('Increment\Common\Payload\Http\PayloadController')->retrieveByParams($item['category']);
+      $result[$i]['additional_info'] = json_decode($item['additional_info']);
+    }
+    $this->response['data'] = $result;
+    $this->response['size'] = sizeof($size);
+
+    return $this->response();
+  }
 }
