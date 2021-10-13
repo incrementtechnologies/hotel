0d1d5fc61c1ba121dd67022136385dd808000693
chloe868 <cherrymae.herrera@student.passerellesnumeriques.org>
2021-10-13 11:08:30 +0800

    Updates

--- addon/src/http/AddOnController.php
+++ addon/src/http/AddOnController.php
@@ -17,7 +17,15 @@ class AddOnController extends APIController
 
     public function retrieve(Request $request) {
 		$data = $request->all();
+		$con = $data['condition'];
+		$sortBy = 'add_ons.'.array_keys($data['sort'])[0];
+		$condition = array(
+			array('add_ons.' . $con[0]['column'], $con[0]['clause'], $con[0]['value']),
+			array('add_ons.' . $con[1]['column'], $con[1]['clause'], $con[1]['value']),
+			array('add_ons.' . $con[2]['column'], $con[2]['clause'], $con[2]['value'])
+		);
 		$results = DB::table('add_ons')
+			->where($condition)
 			->where('deleted_at', '=', null)
 			->limit($data['limit'])
 			->offset($data['offset'])
