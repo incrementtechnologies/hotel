303e7261701d6b172c243fd3b60dbb0c065eb541
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-10-26 14:53:57 +0800

    retrieve pricings

--- room/src/routes/web.php
+++ room/src/routes/web.php
@@ -20,6 +20,7 @@ $route = env('PACKAGE_ROUTE', '').'/pricings/';
 $controller = 'Increment\\Hotel\Room\Http\PricingController@';
 Route::post($route.'create', $controller."create");
 Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'retrieve_pricings', $controller."retrievePricings");
 Route::post($route.'update', $controller."update");
 Route::post($route.'delete', $controller."delete");
 Route::get($route.'test', $controller."test");
