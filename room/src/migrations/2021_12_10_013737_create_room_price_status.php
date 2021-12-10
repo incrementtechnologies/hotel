a3000518181800ced1d33a1e22413adf6feab225
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-12-10 13:46:59 +0800

    creat

--- room/src/migrations/2021_12_10_013737_create_room_price_status.php
+++ room/src/migrations/2021_12_10_013737_create_room_price_status.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRoomPriceStatu extends Migration
+class CreateRoomPriceStatus extends Migration
 {
     /**
      * Run the migrations.
