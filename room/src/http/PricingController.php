795a05fb63f80a45143f736d0f45d39eded58d21
pathran-phantom1823 <parick.pathran23@gmail.com>
2022-06-06 11:58:11 +0800

    updates

--- room/src/http/PricingController.php
+++ room/src/http/PricingController.php
@@ -29,41 +29,41 @@ class PricingController extends APIController
     	return $this->response();
     }
 
-		public function create(Request $request){
-			$data = $request->all();
-			// if(isset($data['addOnPrice'])){
-			// 	$data['regular'] += array_sum($data['addOnPrice']);
-			// }
-			if((int)$data['refundable'] > 0){
-				$data['tax_price'] = $data['tax'] == 1 ? (float)$data['refundable'] + ((ENV('TAX_PRICE')/100) * (float)$data['refundable']) : (float)$data['refundable'];
-			}else{
-				$data['tax_price'] = $data['tax'] == 1 ? (float)$data['regular'] + ((ENV('TAX_PRICE')/100) * (float)$data['regular']) : (float)$data['regular'];
-			}
-			$this->insertDB($data);
-			if($this->response['data']){
-				$priceId = Pricing::leftJoin('rooms as T1', 'T1.id', '=', 'pricings.room_id')->where('pricings.id', '=', $this->response['data'])->first();
-				$prices = Pricing::leftJoin('rooms as T1', 'T1.id', '=', 'pricings.room_id')->where('pricings.regular', '=', $priceId['regular'])->where('pricings.label', '=', $priceId['label'])->where('T1.category', '=', $priceId['category'])->get();
-				
-				$params = array(
-					'price_id' => $this->response['data'],
-					'category_id' => $data['category'],
-					'amount' => $data['tax_price'],
-					'refundable' => $data['refundable'] !== null ? (float)$data['refundable'] : (float)0,
-					'qty' => 1,
-					'status' => 'available'
-				);
-				app('Increment\Hotel\Room\Http\RoomPriceStatusController')->insertPriceStatus($params);
-			}
-			return $this->response();
+	public function create(Request $request){
+		$data = $request->all();
+		// if(isset($data['addOnPrice'])){
+		// 	$data['regular'] += array_sum($data['addOnPrice']);
+		// }
+		if((int)$data['refundable'] > 0){
+			$data['tax_price'] = $data['tax'] == 1 ? (float)$data['refundable'] + ((ENV('TAX_PRICE')/100) * (float)$data['refundable']) : (float)$data['refundable'];
+		}else{
+			$data['tax_price'] = $data['tax'] == 1 ? (float)$data['regular'] + ((ENV('TAX_PRICE')/100) * (float)$data['regular']) : (float)$data['regular'];
 		}
+		$this->insertDB($data);
+		if($this->response['data']){
+			$priceId = Pricing::leftJoin('rooms as T1', 'T1.id', '=', 'pricings.room_id')->where('pricings.id', '=', $this->response['data'])->first();
+			$prices = Pricing::leftJoin('rooms as T1', 'T1.id', '=', 'pricings.room_id')->where('pricings.regular', '=', $priceId['regular'])->where('pricings.label', '=', $priceId['label'])->where('T1.category', '=', $priceId['category'])->get();
+			
+			$params = array(
+				'price_id' => $this->response['data'],
+				'category_id' => $data['category'],
+				'amount' => $data['tax_price'],
+				'refundable' => $data['refundable'] !== null ? (float)$data['refundable'] : (float)0,
+				'qty' => 1,
+				'status' => 'available'
+			);
+			app('Increment\Hotel\Room\Http\RoomPriceStatusController')->insertPriceStatus($params);
+		}
+		return $this->response();
+	}
 
-		public function retrievePricings(Request $request){
-			$data = $request->all();
-			$con = $data['condition'];
-			$result = Pricing::where($con[0]['column'], $con[0]['clause'], $con[0]['value'])->groupBy('label')->get();
-			$this->response['data'] = $result;
-			return $this->response();
-		}
+	public function retrievePricings(Request $request){
+		$data = $request->all();
+		$con = $data['condition'];
+		$result = Pricing::where($con[0]['column'], $con[0]['clause'], $con[0]['value'])->groupBy('label')->get();
+		$this->response['data'] = $result;
+		return $this->response();
+	}
     public function getProduct($productId){
     	$result = Room::where('id', '=', $productId)->first();
     	return ($result) ? $result : null;
@@ -74,75 +74,75 @@ class PricingController extends APIController
       return (sizeof($result) > 0) ? $result : null;
     }
 
-		public function retrieveMaxMin(){
-			$min = Pricing::where('deleted_at', '=', null)->orderBy('tax_price', 'asc')->first();
-			$max = Pricing::where('deleted_at', '=', null)->orderBy('tax_price', 'desc')->first();
-			return array(
-				'min' => $min !== null ? (int)$min['tax_price'] : 0,
-				'max' => $max !== null ? (int)$max['tax_price'] : 0
+	public function retrieveMaxMin(){
+		$min = Pricing::where('deleted_at', '=', null)->orderBy('tax_price', 'asc')->first();
+		$max = Pricing::where('deleted_at', '=', null)->orderBy('tax_price', 'desc')->first();
+		return array(
+			'min' => $min !== null ? (int)$min['tax_price'] : 0,
+			'max' => $max !== null ? (int)$max['tax_price'] : 0
+		);
+	}
+
+	public function retrieveLabel(){
+		$withTax = Pricing::groupBy('label')->where('tax', '=', 1)->get(['id', 'label']);
+		$withoutTax = Pricing::groupBy('label')->where('tax', '=', 0)->get(['id', 'label']);
+		$tempArray = array();
+		if(sizeof($withTax) > 0){
+			for ($i=0; $i <= sizeof($withTax)-1; $i++) {
+				$item = $withTax[$i];
+				$item['label'] = $item['label'].' '.'with tax & fees';	
+				array_push($tempArray, $item);
+			}
+		}
+		if(sizeof($withoutTax) > 0){
+			for ($i=0; $i <= sizeof($withoutTax)-1; $i++){
+				$item = $withoutTax[$i];
+				array_push($tempArray, $item);
+			}
+		}
+		return $tempArray;
+	}
+
+	public function update(Request $request){
+		$data = $request->all();
+		$params = array(
+			'account_id' => $data['account_id'],
+			'room_id' => $data['room_id'],
+			'regular' => $data['regular'],
+			'refundable' => $data['refundable'],
+			'currency' => $data['currency'],
+			'tax' => $data['tax'],
+			'label' => $data['label'],
+			'updated_at' => Carbon::now()
+		);
+		if((int)$data['refundable'] > 0){
+			$params['tax_price'] = $data['tax'] == 1 ? (float)$data['refundable'] + ((ENV('TAX_PRICE')/100) * (float)$data['refundable']) : (float)$data['refundable'];
+		}else{
+			$params['tax_price'] = $data['tax'] == 1 ? (float)$data['regular'] + ((ENV('TAX_PRICE')/100) * (float)$data['regular']) : (float)$data['regular'];
+		}
+		$result = Pricing::where('id', '=', $data['id'])->update($params);
+		if($result){
+			$condition = array(
+				array('price_id', '=', $data['id'])
 			);
-		}
-
-		public function retrieveLabel(){
-			$withTax = Pricing::groupBy('label')->where('tax', '=', 1)->get(['id', 'label']);
-			$withoutTax = Pricing::groupBy('label')->where('tax', '=', 0)->get(['id', 'label']);
-			$tempArray = array();
-			if(sizeof($withTax) > 0){
-				for ($i=0; $i <= sizeof($withTax)-1; $i++) {
-					$item = $withTax[$i];
-					$item['label'] = $item['label'].' '.'with tax & fees';	
-					array_push($tempArray, $item);
-				}
-			}
-			if(sizeof($withoutTax) > 0){
-				for ($i=0; $i <= sizeof($withoutTax)-1; $i++){
-					$item = $withoutTax[$i];
-					array_push($tempArray, $item);
-				}
-			}
-			return $tempArray;
-		}
-
-		public function update(Request $request){
-			$data = $request->all();
-			$params = array(
-				'account_id' => $data['account_id'],
-				'room_id' => $data['room_id'],
-				'regular' => $data['regular'],
-				'refundable' => $data['refundable'],
-				'currency' => $data['currency'],
-				'tax' => $data['tax'],
-				'label' => $data['label'],
-				'updated_at' => Carbon::now()
+			$update = array(
+				'amount' => $params['tax_price'],
+				'refundable' => $data['refundable'] !== null ? (float)$data['refundable'] : (float)0,
+				'category_id' => $data['category_id']
 			);
-			if((int)$data['refundable'] > 0){
-				$params['tax_price'] = $data['tax'] == 1 ? (float)$data['refundable'] + ((ENV('TAX_PRICE')/100) * (float)$data['refundable']) : (float)$data['refundable'];
-			}else{
-				$params['tax_price'] = $data['tax'] == 1 ? (float)$data['regular'] + ((ENV('TAX_PRICE')/100) * (float)$data['regular']) : (float)$data['regular'];
-			}
-			$result = Pricing::where('id', '=', $data['id'])->update($params);
-			if($result){
-				$condition = array(
-					array('price_id', '=', $data['id'])
-				);
-				$update = array(
-					'amount' => $params['tax_price'],
-					'refundable' => $data['refundable'] !== null ? (float)$data['refundable'] : (float)0,
-					'category_id' => $data['category_id']
-				);
-				$updatePriceStatus = app('Increment\Hotel\Room\Http\RoomPriceStatusController')->updateByParams($condition, $update);
-			}
-			$this->response['data'] = $result === 1 ? true : false;
-			return $this->response();
+			$updatePriceStatus = app('Increment\Hotel\Room\Http\RoomPriceStatusController')->updateByParams($condition, $update);
 		}
+		$this->response['data'] = $result === 1 ? true : false;
+		return $this->response();
+	}
 
-		public function retrieveByColumn($column, $value){
-			return Pricing::where($column, '=', $value)->first();
-		}
+	public function retrieveByColumn($column, $value){
+		return Pricing::where($column, '=', $value)->first();
+	}
 
-		public function deleteByColumn($column, $value){
-			return Pricing::where($column, '=', $value)->update(array(
-				'deleted_at' => Carbon::now()
-			));
-		}
+	public function deleteByColumn($column, $value){
+		return Pricing::where($column, '=', $value)->update(array(
+			'deleted_at' => Carbon::now()
+		));
+	}
 }
