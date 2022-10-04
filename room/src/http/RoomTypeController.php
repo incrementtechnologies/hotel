1dc67fc4d61226fda4e2062875bb2ebb3c1f59c2
pathran-phantom1823 <parick.pathran23@gmail.com>
2022-10-04 18:07:15 +0800

    updates

--- room/src/http/RoomTypeController.php
+++ room/src/http/RoomTypeController.php
@@ -222,7 +222,7 @@ class RoomTypeController extends APIController
           $isAvailable = app('Increment\Hotel\Room\Http\AvailabilityController')->isAvailable($item['payload_value'], $data['check_in'], $data['check_out']);
           $cartReservation = app('Increment\Hotel\Room\Http\CartController')->countDailyCarts($item['start_date'], $item['availabilityId'], $item['category_id']);
           if($isAvailable && $cartReservation != $item['limit_per_day']){
-            if(Carbon::parse($item['start_date']) >= Carbon::parse($data['check_in'])){
+            if(Carbon::parse($item['start_date']) <= Carbon::parse($data['check_in']) && Carbon::parse($item['end_date']) >= Carbon::parse($data['check_in'])){
               array_push($result, $temp[$i]);
             }
           }
@@ -306,7 +306,7 @@ class RoomTypeController extends APIController
           }
           $cartReservation = app('Increment\Hotel\Room\Http\CartController')->countDailyCarts($item['start_date'], $item['availabilityId'], $item['categoryId']);
           if($cartReservation != $item['limit_per_day']){
-            if(Carbon::parse($data['filter']['check_in']) >= Carbon::parse($item['start_date'])){
+            if(Carbon::parse($data['filter']['check_in']) >= Carbon::parse($item['start_date']) && Carbon::parse($data['filter']['check_in']) <= Carbon::parse($item['start_date'])){
               array_push($result, $temp[$i]);
             }
           }
